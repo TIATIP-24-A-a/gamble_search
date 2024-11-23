61fd9c4a191e392571e857563fe1af8123434b13
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-11-23 15:13:11 +0100

    implement gamble_search to pass the first test

--- src/gamble_search.py
+++ src/gamble_search.py
@@ -1,2 +1,15 @@
-def gamble_search(array, target):
-    pass
+import random
+
+def gamble_search(array: list[str], target: str) -> int:
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        pivot = random.randint(left, right)
+        if array[pivot] == target:
+            return pivot
+        elif array[pivot] < target:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+
+    return 0
