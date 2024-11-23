f7fa3a32c3a19edbf2a1fbd37cc2fc893e2286e2
Stevic-Nenad <stevic.nenad@icloud.com>
2024-11-23 15:15:41 +0100

    added test with medium sample size

--- src/test/test_gamble_search.py
+++ src/test/test_gamble_search.py
@@ -11,7 +11,10 @@ def medium_array():
 
 @pytest.fixture
 def large_array():
-    return [f"item_{i}" for i in range(10000)]
+    return [f"item_{i}" for i in range(1000000)]
 
 def test_gamble_search_should_return_correct_answer_with_small_sample(small_array):
     assert gamble_search(small_array, "cherry") == 2
+
+def test_gamble_search_should_return_correct_answer_with_medium_sample(medium_array):
+    assert gamble_search(medium_array, "item_50") == 50
