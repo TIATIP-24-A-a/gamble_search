a61f495b4be26a406c9a995653ea56bc69f19ed9
Stevic-Nenad <stevic.nenad@icloud.com>
2024-11-23 15:07:30 +0100

    renamed test to more accurately reflect purpose

--- src/test/test_gamble_search.py
+++ src/test/test_gamble_search.py
@@ -13,5 +13,5 @@ def medium_array():
 def large_array():
     return [f"item_{i}" for i in range(10000)]
 
-def test_gamble_search_should_return_correct_answer(small_array):
+def test_gamble_search_should_return_correct_answer_with_small_sample(small_array):
     assert gamble_search(small_array, "cherry") == 2
